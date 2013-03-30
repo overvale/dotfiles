6cf2bedf9903db9389bda022814144d09aa68b77
Oliver Taylor <olivertaylor@me.com>
2013-03-30 13:14:15 -0700

    Added settings for KeyRemap4MacBook, other misc updates

---
+++ KeyRemap4MacBook/README.md
@@ -0,0 +1,8 @@
+KeyRemap4MacBook
+================
+
+My cusomizations for [KeyRemap4MacBook](http://pqrs.org/macosx/keyremap4macbook/).
+
+1. Remaps right-option to control+shift+option+command
+2. Remaps left-control to F5 when tapped without another key, behaves as normal control otherwise.
+3. Disables incorrect shift+letter combinations (from: [Steve Losh](http://stevelosh.com/blog/2012/10/a-modern-space-cadet/#shift-key-training-wheels))
---
+++ symlink-dotfiles.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# Use this script to setup a new mac with your dot-files
+
+ln -s /Users/oliver/Dropbox/code/bin/dotfiles/bash/bash_profile .bash_profile
+ln -s /Users/oliver/Dropbox/code/bin/dotfiles/bashrc .bashrc
+ln -s /Users/oliver/Dropbox/code/bin/dotfiles/gitconfig .gitconfig
+ln -s /Users/oliver/Dropbox/code/bin/dotfiles/gitignore_global .gitignore_global
+ln -s /Users/oliver/Dropbox/code/bin/dotfiles/slate .slate
+ln -s /Users/oliver/Dropbox/code/bin/dotfiles/vim .vim
+ln -s /Users/oliver/Dropbox/code/bin/dotfiles/vim/vimrc .vimrc
+ln -s /Users/oliver/Dropbox/code/bin/dotfiles/zshrc .zshrc
+
+ln -s /Users/oliver/Dropbox/code/bin/dotfiles/KeyRemap4MacBook /Users/oliver/Library/Application\ Support/KeyRemap4MacBook

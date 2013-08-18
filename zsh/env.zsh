e2adb9f6fc6a6af18daeca39d253b0b51ff771ac
Oliver Taylor <olivertaylor@me.com>
2013-08-17 18:28:50 -0700

    Changes cd aliases to “marks.zsh”

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,7 +1,6 @@
 # Set some basic variables
 export PATH=$PATH:~/code/bin:~/code/dotfiles/bin
 export EDITOR=/usr/local/bin/bbedit
-export DOT=$HOME/code/dotfiles
 
 # set a search directory for cd in addition to the working dir
 export CDPATH=.:~

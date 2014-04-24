a0c4b11fd697afedc2b9ad320433f8e0ad27d254
Oliver Taylor <olivertaylor@me.com>
2014-04-23 23:41:57 -0700

    updated bootstrap to reflect my current setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,55 +1,12 @@
-#!/bin/zsh
-
-# Use this script to setup a new mac with your dot-files
-# ensure you run it from $HOME, and be very careful with your data
+# Use this document to setup a new Mac.
+# This is not meant to be run as a shell-script,
+# rather, as a guide.
 
 # Sign in to iCloud
 # Sign in to iTunes Match
-# 
-# Bootstrap my dotfiles with symlinks
-# Change login shell to zsh
-# 
-# Install apps:
-# Slate
-# KeyRemap4MacBook
-# Dropbox
-# iTerm
-# Lightroom
-# Chrome
-# Kaleidoscope
-# Pixelmator
-# Super Duper
-# The Unarchiver
-# Transmit
-# VLC
-# Writer Pro
-# 
-# Once everything's working...
-# Install Arq backup
-
-
-# Ask for the administrator password upfront
-sudo -v
 
 # Installs the developer command line tools
 xcode-select --install
-
-ln -s ~/code/dotfiles/git/gitconfig .gitconfig
-ln -s ~/code/dotfiles/git/gitignore_global .gitignore_global
-
-ln -s ~/code/dotfiles/zsh .zsh
-ln -s ~/.zsh/zshrc .zshrc
-
-ln -s ~/code/dotfiles/vim .vim
-ln -s ~/.vim/vimrc .vimrc
-
-ln -s ~/code/dotfiles/slate .slate
-ln -s ~/code/dotfiles/KeyRemap4MacBook ~/Library/Application\ Support/KeyRemap4MacBook
-
-
-
-# A few personal preferences...
-# Stolen from: https://github.com/mathiasbynens/dotfiles
 
 # set default shell
 chsh -s /opt/local/bin/zsh
@@ -59,3 +16,35 @@ defaults write com.apple.helpviewer DevMode -bool true
 
 # Avoid creating .DS_Store files on network volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Install apps:
+# Adobe Lightroom
+# BBEdit
+# Dropbox
+# Google Chrome
+# iTerm2
+# Kaleidoscope
+# Keyboard Maestro
+# KeyRemap4Macbook
+# Launchbar
+# Name Mangler
+# Patterns
+# Pixelmator
+# SuperDuper
+# The Unarchiver
+# Transmit
+
+# Symlink a bunch of things - run from $HOME
+ln -s ~/code/dotfiles/git/gitconfig .gitconfig
+ln -s ~/code/dotfiles/git/gitignore_global .gitignore_global
+ln -s ~/code/dotfiles/zsh .zsh
+ln -s ~/.zsh/zshrc .zshrc
+ln -s ~/code/dotfiles/vim .vim
+ln -s ~/.vim/vimrc .vimrc
+ln -s ~/code/dotfiles/KeyRemap4MacBook ~/Library/Application\ Support/KeyRemap4MacBook
+
+# install Arq
+# http://www.haystacksoftware.com/arq/
+# and restore anything that wasn't automatically restored by
+# cloud services (iCloud, Dropbox, etc.)
+

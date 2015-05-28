5a849fbade0a0559dcc6b9bb895222cc4e7fb86b
Oliver Taylor <mail@olivertaylor.net>
2015-05-27 23:15:06 -0700

    Minor changes

--- zsh/marks.zsh
+++ zsh/marks.zsh
@@ -1,12 +1,12 @@
 # from <http://jeroenjanssens.com/2013/08/16/quickly-navigate-your-filesystem-from-the-command-line.html>
 export MARKPATH=$HOME/.marks
-function jump { 
+function jmp {
     cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
 }
-function mark { 
+function mark {
     mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
 }
-function unmark { 
+function unmark {
     rm -i "$MARKPATH/$1"
 }
 function marks {

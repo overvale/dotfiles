be270ec8e095666b1746cd38eb230e4304138b30
Oliver Taylor <mail@olivertaylor.net>
2016-05-31 03:52:28 -0700

    Tweaks emacs config

--- emacs/settings.el
+++ emacs/settings.el
@@ -22,11 +22,11 @@
 
 ;; Per mode settings
 (add-hook 'text-mode-hook 'turn-on-flyspell)
-(add-hook 'org-mode-hook
-  (lambda ()
-	(org-indent-mode t)
-	)
-  t)
+;;(add-hook 'org-mode-hook
+;;  (lambda ()
+;;	(org-indent-mode t)
+;;	)
+;;  t)
 
 ;; Font settings
 (set-face-attribute 'default nil
@@ -40,3 +40,9 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time

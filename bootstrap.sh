59f38720a16d03d392c8f5562e005de9c74e13f9
Oliver Taylor <mail@olivertaylor.net>
2016-11-16 20:41:37 -0800

    Tweaks settings based on today's move

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,7 @@
 
 # Check for software updates
 # Install ad blocking
-# Install Little Snitch
-# Create separate local password (iCloud password used by default)
 # Change caps-lock to control
-# Turn on trackpad tap to click
-# Disable guest account
 
 # Copy documents from backup
 
@@ -21,6 +17,8 @@ xcode-select --install
 # set default shell
 chsh -s /bin/zsh
 
+# Set up ssh keys
+
 # install homebrew - worth checking hompage
 # for, potentially, updated instructions
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -29,54 +27,34 @@ brew install vim
 brew install fzf
 brew install par
 brew install git
-brew install git-lfs
 brew install tree
 brew install ranger
-brew install tmux
-brew install reattach-to-user-namespace
 brew install pandoc
 brew install zsh
 brew install ispell
-brew install awscli
-brew install urlview
-brew install contacts
-
-git lfs install
-easy_install readability-lxml
+brew install emacs --with-cocoa
 
 # Install ~/code/other-repos from backup
 
 # Install princeXML
 # make install textplay (symlink)
-# Set up ssh keys
-
-# Set Help Viewer windows to non-floating mode
-defaults write com.apple.helpviewer DevMode -bool true
-
-# Avoid creating .DS_Store files on network volumes
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
 # pull-down your dotfiles, or copy from backup
 
 mkdir ~/.config
 
 # Symlink a bunch of things - run from $HOME
-ln -s ~/code/dotfiles/git/gitconfig .gitconfig
-ln -s ~/code/dotfiles/git/gitignore_global .gitignore_global
-ln -s ~/code/dotfiles/zsh .zsh
-ln -s ~/code/dotfiles/zsh/zshrc .zshrc
-ln -s ~/code/dotfiles/vim .config/nvim
-ln -s ~/code/dotfiles/vim .vim
-ln -s ~/code/dotfiles/Karabiner ~/Library/Application\ Support/Karabiner
-ln -s ~/code/dotfiles/tmux.conf .tmux.conf
-ln -s ~/code/dotfiles/slate .slate
-ln -s ~/code/dotfiles/emacs ~/.emacs.d
-ln -s ~/code/dotfiles/mutt/mailcap .mailcap
-ln -s ~/code/dotfiles/mutt .mutt
-ln -s ~/code/dotfiles/mutt/muttrc .muttrc
+ln -s ~/code/dotfiles/git/gitconfig .gitconfig;
+ln -s ~/code/dotfiles/git/gitignore_global .gitignore_global;
+ln -s ~/code/dotfiles/zsh .zsh;
+ln -s ~/code/dotfiles/zsh/zshrc .zshrc;
+ln -s ~/code/dotfiles/vim .vim;
+ln -s ~/code/dotfiles/emacs ~/.emacs.d;
 
 # Install vim plugin manager + plugins
 # Reconstruct ~/.marks directory
+
+# Install dropbox
 
 # Install apps:
 
@@ -85,20 +63,12 @@ ln -s ~/code/dotfiles/mutt/muttrc .muttrc
 # BBEdit
 # Emacs
 # Google Chrome
-# Kaleidoscope
-# Karabiner
-# Launchbar
-# Little Snitch
 # Name Mangler (Mac App Store)
 # Omnifocus
 # Pixelmator (Mac App Store)
 # QuickTime Player 7
-# Shortcat
 # Slack
-# Slate
 # SuperDuper
 # Transmit
-# iA Writer
-# iWork
 
 # Install fonts from server
--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,11 @@
 ;; This loads my settings file, leaving the init.el file for interactive customizations
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (load-library "~/.emacs.d/settings.el")
 
 (custom-set-variables

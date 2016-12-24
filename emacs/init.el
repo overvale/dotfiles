e8575343c2b0ca317125886f9ff2dcaeafc55678
Oliver Taylor <mail@olivertaylor.net>
2016-12-24 01:53:13 -0800

    Colors changes

--- emacs/init.el
+++ emacs/init.el
@@ -13,21 +13,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-enabled-themes (quote (ample-theme)))
  '(custom-safe-themes
    (quote
-    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "7557aa0d3854c7e910121ba2ef94f4c4e70de7d32ddebb609719f545f7f7be0d" "ad950f1b1bf65682e390f3547d479fd35d8c66cafa2b8aa28179d78122faa947" "db210d68e7231e5f2361cd8f6e6bc261f4ea02a72e695a5166eee940e7530a76" "0153cc571e0e2ebdb93a1702a2a77c81ef6d8d252b64d6b53e85502f66e66a8c" "1867e0e8f67310ded7db2ac1d70b80c2b75f2c2bf9baca5ee2d5360236f54d3c" "e21d7b25ca2bf460e5b78d5aaf43dcc0b38d955218755df59c4ffd0419983af1" default)))
- '(fci-rule-color "#f8fce8")
- '(hl-paren-background-colors (quote ("#e8fce8" "#c1e7f8" "#f8e8e8")))
- '(hl-paren-colors (quote ("#40883f" "#0287c8" "#b85c57")))
- '(sml/active-background-color "#98ece8")
- '(sml/active-foreground-color "#424242")
- '(sml/inactive-background-color "#4fa8a8")
- '(sml/inactive-foreground-color "#424242"))
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "8b30636c9a903a9fa38c7dcf779da0724a37959967b6e4c714fdc3b3fe0b8653" "0153cc571e0e2ebdb93a1702a2a77c81ef6d8d252b64d6b53e85502f66e66a8c" default)))
+ '(package-selected-packages (quote (spacemacs-theme magit helm evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

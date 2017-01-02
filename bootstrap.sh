f4a2dcb4be2ae6dfc69fe87cfd4a37245effe9bb
Oliver Taylor <mail@olivertaylor.net>
2017-01-02 10:56:46 -0800

    Changes default shell to bash

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,5 @@
 # Use this document to setup a new Mac.
-# This is not meant to be run as a shell-script,
-# rather, as a guide.
+# This is not meant to be run as a shell-script, rather, as a guide.
 
 # Sign in to iCloud
 # Sign in to iTunes Match
@@ -9,18 +8,18 @@
 # Install ad blocking
 # Change caps-lock to control
 
-# Copy documents from backup
+# Copy files from backup
+# Install ~/code/other-repos from backup
+# pull-down your dotfiles, or copy from backup
 
 # Installs the developer command line tools
 xcode-select --install
 
-# set default shell
-chsh -s /bin/zsh
+sudo echo "Welcome to UNIX!" > /etc/motd
 
 # Set up ssh keys
 
-# install homebrew - worth checking hompage
-# for, potentially, updated instructions
+# install homebrew - worth checking homepage for, potentially, updated instructions
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install emacs --with-cocoa
@@ -31,24 +30,20 @@ brew install pandoc
 brew install par
 brew install tree
 brew install vim
-brew install zsh
-
-# Install ~/code/other-repos from backup
 
 # Install princeXML
 # make install textplay (symlink)
-
-# pull-down your dotfiles, or copy from backup
 
 mkdir ~/.config
 
 # Symlink a bunch of things - run from $HOME
 ln -s ~/code/dotfiles/git/gitconfig .gitconfig;
 ln -s ~/code/dotfiles/git/gitignore_global .gitignore_global;
-ln -s ~/code/dotfiles/zsh/zshrc .zshrc;
 ln -s ~/code/dotfiles/vim .vim;
 ln -s ~/code/dotfiles/emacs ~/.emacs.d;
 ln -s ~/code/dotfiles/hammerspoon ~/.hammerspoon;
+ln -s ~/code/dotfiles/bashrc ~/.bashrc
+ln -s ~/code/dotfiles/bashrc ~/.bash_profile
 
 # Install vim plugin manager + plugins
 # Reconstruct ~/.marks directory
@@ -76,7 +71,7 @@ ln -s ~/code/dotfiles/hammerspoon ~/.hammerspoon;
 
 # ----------------------------------------------------------
 
-# They keyboard setup is rather spesific.
+# They keyboard setup is rather specific.
 # At the moment Karabiner doesn't work with MacOS Sierra,
 # so I'm using Karabiner Elements to remap 2 things:
 # 1. Caps lock -> Left control
@@ -87,4 +82,4 @@ ln -s ~/code/dotfiles/hammerspoon ~/.hammerspoon;
 # c. Right option -> hyper
 #
 # At the moment I'm faking Caps -> Escape/Control via Karabiner + Hammerspoon
-# but there's some lag, which is dissapointing, and Return -> Return/Control is impossible.
+# but there's some lag, which is disappointing, and Return -> Return/Control is impossible.

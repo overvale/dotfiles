cb07b7e2941b7407130d771b15c836ef2d5b4b8a
Oliver Taylor <mail@olivertaylor.net>
2017-03-30 09:47:26 -0700

    Standardized shortcuts for Safari, Chrome, Slack

--- hammerspoon/windows.lua
+++ hammerspoon/windows.lua
@@ -12,7 +12,7 @@ hs.hotkey.bind(half_hyper, "'", function() hs.fnutils.map(hs.window.visibleWindo
 
 hs.hotkey.bind(half_hyper, 'm', hs.grid.maximizeWindow)
 
-hs.hotkey.bind(half_hyper, 'c', function() hs.window.centerOnScreen(hs.window.focusedWindow()) end)
+--hs.hotkey.bind(half_hyper, 'c', function() hs.window.centerOnScreen(hs.window.focusedWindow()) end)
 
 hs.hotkey.bind(half_hyper, 'j', hs.grid.pushWindowDown)
 hs.hotkey.bind(half_hyper, 'k', hs.grid.pushWindowUp)

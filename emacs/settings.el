4eb533f6fffe4b4b0525a75a2cdb80a0cb4b13a2
Oliver Taylor <olivertaylor@me.com>
2020-07-03 14:31:56 -0700

    settings tweaking

--- emacs/settings.el
+++ emacs/settings.el
@@ -1,5 +1,5 @@
-;; Sane defaults from: https://github.com/rougier/elegant-emacs
-;; --------------------------------------------------------
+;;; Sane defaults from: https://github.com/rougier/elegant-emacs
+;;; --------------------------------------------------------
 
 ;; garbage collection
 (setq gc-cons-threshold (* 100 1024 1024))
@@ -17,103 +17,110 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 
-;; General Settings
-;; --------------------------------------------------------
+
+;;; General Settings
+;;; --------------------------------------------------------
 
 (tool-bar-mode 0)
 (tooltip-mode  0)
 (scroll-bar-mode 0)
-
+(set-default 'cursor-type 'bar)
+(global-visual-line-mode t)                     ; word-wrap
 (setq backup-directory-alist `(("." . "~/.emacs.d/saves")))
 (setq ispell-program-name "/usr/local/bin/aspell")
 (setq ispell-list-command "list")
-(set-default 'cursor-type 'bar)
-(global-visual-line-mode t) ;; word-wrap
-
 (add-to-list 'load-path "~/.emacs.d/colors/")
 
 (which-key-mode)
 (global-undo-tree-mode)
+(show-paren-mode)
+
+;; counsel settings
 (counsel-mode)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(setq ivy-count-format "(%d/%d) ")
 
-;; Keybindings
-;; --------------------------------------------------------
 
+;;; Keybindings
+;;; --------------------------------------------------------
+
+;; remap C-RET to C-c for faster prefixed keybindings
+(global-set-key
+ (kbd "C-<return>")
+ (lookup-key global-map
+	     (kbd "C-c")))
+
+;; replace search with swiper
+(global-set-key (kbd "C-s") 'swiper)
+
+;; upgrade to hippe-expand
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+;; Quick Shortcuts
+(global-set-key (kbd "C-c o") 'other-window)
+(global-set-key (kbd "C-c b") 'counsel-switch-buffer)
+(global-set-key (kbd "C-c f") 'counsel-fzf)
+(global-set-key (kbd "C-c l") 'swiper-all)
+(global-set-key (kbd "C-c x") 'counsel-M-x)
+
+;; quickly open my settings (this) file
 (defun settings-edit-file ()
   (interactive)
   (find-file "~/dot/emacs/settings.el"))
-
 
 ;; quickly kill current buffer
 (defun custom/kill-this-buffer ()
   (interactive) (kill-buffer (current-buffer)))
 (global-set-key (kbd "C-x k") 'custom/kill-this-buffer)
 
-
 ;; Quickly switch to scratch buffer with Cmd+0:
-(global-set-key (kbd "s-0") (lambda ()
-                              (interactive)
-                              (if (string= (buffer-name) "*scratch*")
-                                  (previous-buffer)
-                                (switch-to-buffer "*scratch*"))))
+(global-set-key
+ (kbd "s-0")
+ (lambda ()
+   (interactive)
+   (if (string= (buffer-name) "*scratch*")
+       (previous-buffer)
+     (switch-to-buffer "*scratch*"))))
 
-(global-set-key (kbd "s-<right>") 'next-buffer)
-(global-set-key (kbd "s-<left>") 'previous-buffer)
-
-;; counsel settings
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-(setq ivy-count-format "(%d/%d) ")
-
-(global-set-key (kbd "C-s") 'swiper)
-
+;; Move Lines
 (defmacro save-column (&rest body)
   `(let ((column (current-column)))
      (unwind-protect
          (progn ,@body)
        (move-to-column column))))
 (put 'save-column 'lisp-indent-function 0)
-
-;; quick shortcuts
-(global-set-key (kbd "C-c o") 'other-window)
-(global-set-key (kbd "C-c b") 'counsel-switch-buffer)
-(global-set-key (kbd "C-c f") 'counsel-fzf)
-(global-set-key (kbd "C-c l") 'swiper-all)
-(global-set-key (kbd "C-c SPC") 'counsel-M-x)
-
-;; Move Lines
 (defun move-line-up ()
   (interactive)
   (save-column
     (transpose-lines 1)
     (forward-line -2)))
-
 (defun move-line-down ()
   (interactive)
   (save-column
     (forward-line 1)
     (transpose-lines 1)
     (forward-line -1)))
-
 (global-set-key (kbd "M-<up>") 'move-line-up)
 (global-set-key (kbd "M-<down>") 'move-line-down)
 
-;; Shortcuts
-;; --------------------------------------------------------
 
-;; Set command to super
+;;; Mac Shortcuts
+;;; --------------------------------------------------------
+
+;; Command to super
 (setq mac-right-command-modifier 'super)
 (setq mac-command-modifier 'super)
 
-;; Set option to meta
+;; Reft-option to meta for commands
 (setq mac-option-modifier 'meta)
 (setq mac-left-option-modifier 'meta)
 
-;; Set right option to option
+;; Right-option to option, for special characters: ¡™£¢∞§¶•ªº
 (setq mac-right-option-modifier 'meta)
 (setq mac-right-option-modifier 'nil)
 
-;; Mac save, undo, cut, copy, paste
+;; Mac-like save, undo, cut, copy, paste, etc.
 (global-set-key (kbd "s-s") 'save-buffer)
 (global-set-key (kbd "s-z") 'undo-tree-undo)
 (global-set-key (kbd "s-Z") 'undo-tree-redo)
@@ -122,26 +129,29 @@
 (global-set-key (kbd "s-v") 'yank)
 (global-set-key (kbd "s-q") 'save-buffers-kill-terminal)
 (global-set-key (kbd "s-w") 'custom/kill-this-buffer)
+(global-set-key (kbd "M-s-<right>") 'next-buffer)
+(global-set-key (kbd "M-s-<left>") 'previous-buffer)
 
-;; Apperance
-;; --------------------------------------------------------
+
+;;; Apperance
+;;; --------------------------------------------------------
 
 ;; Font settings
 ;; switch to non-monospace with 'variable-pitch-mode'
 (set-face-attribute 'default nil
-                    :family "SF Mono" :height 130 :weight 'normal)
+                    :family "SF Mono" :height 130 :weight 'light)
 (set-face-attribute 'variable-pitch nil
-		    :family "New York Medium" :height 140 :weight 'normal)
+		    :family "New York Medium" :height 140 :weight 'light)
 
 (load-theme 'gruvbox)
 
-;; Org Mode
-;; --------------------------------------------------------
 
-;; When activating org-mode, turn on indent and variable-pitch
+;;; Org Mode
+;;; --------------------------------------------------------
+
+;; When org-mode, turn on variable-pitch and spelling
 (add-hook 'org-mode-hook
   (lambda ()
-    ;;(org-indent-mode t)
     (variable-pitch-mode t)
     (flyspell-mode t)
 	)

71b1454dcab68fa79771cfa2d6aeca7f80a4ac27
Oliver Taylor <olivertaylor@me.com>
2020-07-03 21:31:04 -0700

    Adds toggle-window-split function

--- emacs/settings.el
+++ emacs/settings.el
@@ -169,3 +169,35 @@
 	      "~/Documents/ingenuity/ingenuity.org"
 	      "~/Documents/ingenuity/ing_log.org"
 	      )))
+
+
+;;; Toggle Window Split
+;;; --------------------------------------------------------
+
+(defun toggle-window-split ()
+  "Toggle between vertical and horizontal split."
+  ;; Source: https://www.emacswiki.org/emacs/ToggleWindowSplit.
+  ;; Author: Jeff Dwork
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))

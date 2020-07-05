e9cbaa359d420d7cdb616272062c584613abec30
Oliver Taylor <olivertaylor@me.com>
2020-07-05 14:30:03 -0700

    Refactors emacs settings, adds a few Mac-standard things

--- emacs/settings.el
+++ emacs/settings.el
@@ -1,4 +1,4 @@
-;;; Sane defaults from: https://github.com/rougier/elegant-emacs
+;;; General Settings
 ;;; --------------------------------------------------------
 
 ;; garbage collection
@@ -17,52 +17,85 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 
-
-;;; General Settings
-;;; --------------------------------------------------------
-
 (tool-bar-mode 0)
-(tooltip-mode  0)
-(scroll-bar-mode 0)
 (set-default 'cursor-type 'bar)
-(global-visual-line-mode t)                     ; word-wrap
+(global-visual-line-mode t) ; word-wrap
 (setq backup-directory-alist `(("." . "~/.emacs.d/saves")))
 (setq ispell-program-name "/usr/local/bin/aspell")
 (setq ispell-list-command "list")
 (add-to-list 'load-path "~/.emacs.d/colors/")
 
-(which-key-mode)
-(global-undo-tree-mode)
-(show-paren-mode)
+(which-key-mode t)
+(global-undo-tree-mode t)
+(show-paren-mode t)
+(delete-selection-mode t)
 
 ;; counsel settings
-(counsel-mode)
+(counsel-mode t)
 (setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
 (setq ivy-count-format "(%d/%d) ")
 
+;;; Mac-Like Settings
+;; Command to super
+(setq mac-right-command-modifier 'super)
+(setq mac-command-modifier 'super)
 
-;;; Keybindings
+;; Left-option to meta for commands
+(setq mac-option-modifier 'meta)
+(setq mac-left-option-modifier 'meta)
+
+;; Right-option to option, for special characters: ¡™£¢∞§¶•ªº
+(setq mac-right-option-modifier 'meta)
+(setq mac-right-option-modifier 'nil)
+
+;;; Apperance
 ;;; --------------------------------------------------------
 
-;; remap C-RET to C-c for faster prefixed keybindings
-(global-set-key
- (kbd "C-<return>")
- (lookup-key global-map
-	     (kbd "C-c")))
+;; Font settings
+;; switch to non-monospace with 'variable-pitch-mode'
+(set-face-attribute 'default nil
+                    :family "IBM Plex Mono" :height 130 :weight 'normal)
+(set-face-attribute 'variable-pitch nil
+		    :family "IBM Plex Serif" :height 160 :weight 'normal)
 
-;; replace search with swiper
-(global-set-key (kbd "C-s") 'swiper)
+(load-theme 'gruvbox-light-hard t)
 
-;; upgrade to hippe-expand
-(global-set-key (kbd "M-/") 'hippie-expand)
 
-;; Quick Shortcuts
-(global-set-key (kbd "C-c o") 'other-window)
-(global-set-key (kbd "C-c b") 'counsel-switch-buffer)
-(global-set-key (kbd "C-c f") 'counsel-fzf)
-(global-set-key (kbd "C-c l") 'swiper-all)
-(global-set-key (kbd "C-c x") 'counsel-M-x)
+;;; Mode Settings
+;;; --------------------------------------------------------
+
+;; When markdown-mode, turn on variable-pitch and spelling
+(add-hook 'markdown-mode-hook
+  (lambda ()
+    (variable-pitch-mode t)
+    (flyspell-mode t)
+	)
+  t)
+
+;; When org-mode, turn on variable-pitch and spelling
+(add-hook 'org-mode-hook
+  (lambda ()
+    (variable-pitch-mode t)
+    (flyspell-mode t)
+	)
+  t)
+
+(setq org-todo-keywords
+  '((sequence "TODO" "DELG" "|" "OMIT" "DONE")))
+
+(setq org-agenda-files
+      (quote (
+	      "~/Documents/life.org"
+	      "~/Documents/life_log.org"
+	      "~/Documents/ingenuity/ingenuity.org"
+	      "~/Documents/ingenuity/ing_log.org"
+	      )))
+
+
+
+;;; Functions
+;;; --------------------------------------------------------
 
 ;; quickly open my settings (this) file
 (defun settings-edit-file ()
@@ -71,12 +104,11 @@
 
 ;; quickly kill current buffer
 (defun custom/kill-this-buffer ()
-  (interactive) (kill-buffer (current-buffer)))
-(global-set-key (kbd "C-x k") 'custom/kill-this-buffer)
+  (interactive)
+  (kill-buffer (current-buffer)))
 
 ;; Quickly switch to scratch buffer with Cmd+0:
-(global-set-key
- (kbd "s-0")
+(global-set-key (kbd "s-0")
  (lambda ()
    (interactive)
    (if (string= (buffer-name) "*scratch*")
@@ -101,78 +133,6 @@
     (forward-line 1)
     (transpose-lines 1)
     (forward-line -1)))
-(global-set-key (kbd "M-<up>") 'move-line-up)
-(global-set-key (kbd "M-<down>") 'move-line-down)
-
-
-;;; Mac Shortcuts
-;;; --------------------------------------------------------
-
-;; Command to super
-(setq mac-right-command-modifier 'super)
-(setq mac-command-modifier 'super)
-
-;; Reft-option to meta for commands
-(setq mac-option-modifier 'meta)
-(setq mac-left-option-modifier 'meta)
-
-;; Right-option to option, for special characters: ¡™£¢∞§¶•ªº
-(setq mac-right-option-modifier 'meta)
-(setq mac-right-option-modifier 'nil)
-
-;; Mac-like save, undo, cut, copy, paste, etc.
-(global-set-key (kbd "s-s") 'save-buffer)
-(global-set-key (kbd "s-z") 'undo-tree-undo)
-(global-set-key (kbd "s-Z") 'undo-tree-redo)
-(global-set-key (kbd "s-x") 'kill-region)
-(global-set-key (kbd "s-c") 'kill-ring-save)
-(global-set-key (kbd "s-v") 'yank)
-(global-set-key (kbd "s-q") 'save-buffers-kill-terminal)
-(global-set-key (kbd "s-w") 'custom/kill-this-buffer)
-(global-set-key (kbd "M-s-<right>") 'next-buffer)
-(global-set-key (kbd "M-s-<left>") 'previous-buffer)
-
-
-;;; Apperance
-;;; --------------------------------------------------------
-
-;; Font settings
-;; switch to non-monospace with 'variable-pitch-mode'
-(set-face-attribute 'default nil
-                    :family "SF Mono" :height 130 :weight 'light)
-(set-face-attribute 'variable-pitch nil
-		    :family "New York Medium" :height 140 :weight 'light)
-
-(load-theme 'gruvbox)
-
-
-;;; Org Mode
-;;; --------------------------------------------------------
-
-;; When org-mode, turn on variable-pitch and spelling
-(add-hook 'org-mode-hook
-  (lambda ()
-    (variable-pitch-mode t)
-    (flyspell-mode t)
-	)
-  t)
-
-(global-set-key "\C-ca" 'org-agenda)
-
-(setq org-todo-keywords
-  '((sequence "TODO" "DELG" "|" "OMIT" "DONE")))
-
-(setq org-agenda-files
-      (quote (
-	      "~/Documents/life.org"
-	      "~/Documents/life_log.org"
-	      "~/Documents/ingenuity/ingenuity.org"
-	      "~/Documents/ingenuity/ing_log.org"
-	      )))
-
-
-;;; Toggle Window Split
-;;; --------------------------------------------------------
 
 (defun toggle-window-split ()
   "Toggle between vertical and horizontal split."
@@ -201,3 +161,48 @@
           (set-window-buffer (next-window) next-win-buffer)
           (select-window first-win)
           (if this-win-2nd (other-window 1))))))
+
+
+;;; Keybindings
+;;; --------------------------------------------------------
+
+(global-set-key (kbd "C-x k") 'custom/kill-this-buffer)
+
+(define-key key-translation-map (kbd "ESC") (kbd "C-g"))
+
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+;; Quick Shortcuts
+(global-set-key (kbd "C-c o") 'other-window)
+(global-set-key (kbd "C-c v") 'split-window-right)
+(global-set-key (kbd "C-c s") 'split-window-below)
+
+(global-set-key (kbd "C-c b") 'counsel-switch-buffer)
+(global-set-key (kbd "C-c f") 'counsel-fzf)
+(global-set-key (kbd "C-c l") 'swiper-all)
+(global-set-key (kbd "C-c x") 'counsel-M-x)
+(global-set-key (kbd "C-c t") 'counsel-load-theme)
+
+(global-set-key (kbd "M-s-<right>") 'next-buffer)
+(global-set-key (kbd "M-s-<left>") 'previous-buffer)
+
+(global-set-key (kbd "M-<up>") 'move-line-up)
+(global-set-key (kbd "M-<down>") 'move-line-down)
+
+(global-set-key (kbd "s-a") 'org-agenda)
+
+;; Mac-like save, undo, cut, copy, paste, etc.
+(global-set-key (kbd "s-o") 'counsel-find-file)
+(global-set-key (kbd "s-s") 'save-buffer)
+(global-set-key (kbd "s-z") 'undo-tree-undo)
+(global-set-key (kbd "s-Z") 'undo-tree-redo)
+(global-set-key (kbd "s-x") 'kill-region)
+(global-set-key (kbd "s-c") 'kill-ring-save)
+(global-set-key (kbd "s-v") 'yank)
+(global-set-key (kbd "s-q") 'save-buffers-kill-terminal)
+(global-set-key (kbd "s-w") 'custom/kill-this-buffer)
+(global-set-key (kbd "s-<right>") (kbd "C-e"))
+(global-set-key (kbd "s-<left>") (kbd "C-a"))
+(global-set-key (kbd "s-<up>") (kbd "M-<"))
+(global-set-key (kbd "s-<down>") (kbd "M->"))

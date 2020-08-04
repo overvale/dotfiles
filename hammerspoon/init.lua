aede69b4bf6394e8e6d2235e3f85e06919c635c3
Oliver Taylor <olivertaylor@me.com>
2020-08-04 15:32:45 -0700

    Moves ctrl-word-movement to hammerspoon, adds fn+arrow delete
    My ctrl-word-modement remaps are now available system-wide. Without
    any complex hammerspoon code for checking if Emacs was the frontmost
    application. To make this work I just needed to verify that Emacs
    understood all the keys hammerspoon was sending. Which was trivial.So
    this is a much simpler implimentation.
    
    The one downside is that I can no longer use ^u and ^w in Terminal
    because hammerspoon sends cmd+delete and alt+delete instead. But
    that's a minor annoyance since I spend most of my days in Emacs now.
    
    Also turns fn+left/right into delete/forward-delete, and allows for
    the correct alt and cmd modifiers. This is really elegant. If you're
    moving/selecting with the arrow keys already you can just add the 'fn'
    modifier, just like you would with 'shift' to delete text. Beautifully
    simple.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,6 @@
 --[[ Info ---------------------------------------------------------------------
 
 KeyCodes:
-
 f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15,
 f16, f17, f18, f19, f20, pad, pad*, pad+, pad/, pad-, pad=,
 pad0, pad1, pad2, pad3, pad4, pad5, pad6, pad7, pad8, pad9,
@@ -10,11 +9,14 @@ home, pageup, forwarddelete, end, pagedown, left, right, down, up,
 shift, rightshift, cmd, rightcmd, alt, rightalt, ctrl, rightctrl,
 capslock, fn
 
-A lot the below was taken from: github.com/jasonrudolph/keyboard
+http://github.com/jasonrudolph/keyboard
+http://github.com/dbmrq/dotfiles/
 
-]]
+---------------------------------------------------------------------------- ]]
+
 
 -- [ Automatically reload the config when the path changes ] ------------------
+
 function reloadConfig(files)
     doReload = false
     for _,file in pairs(files) do
@@ -28,53 +30,14 @@ function reloadConfig(files)
 end
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 
--- custom function to make keyStrokes easier to type
-keyUpDown = function(modifiers, key)
-  hs.eventtap.keyStroke(modifiers, key, 0)
-end
 
--- [ Keyboard Navigation, NOT EMACS ] ----------------------------------------------------
-
--- This code binds a series of keys when the frontmost application is NOT emacs.
--- It does this by watching the frontmost application and enabling/disabling
--- a series of bindings.
--- https://github.com/Hammerspoon/hammerspoon/issues/2081#issuecomment-668283868
+-- [ Functions ] -----------------------------------------------------------------
 
 -- this is just to make it easier to type 'keyStroke'
-local function keyBind(modifiers, key)
+local function keyStrike(modifiers, key)
   return function() hs.eventtap.keyStroke(modifiers, key, 0) end
 end
 
--- Create a modal hotkey object with an absurd triggering hotkey,
--- since it will never be triggered from the keyboard
-hotkeys = hs.hotkey.modal.new({"cmd", "shift", "alt"}, "F19")
-
--- Place all your bindings here
-hotkeys:bind({'ctrl'}, ',', keyBind({'alt'}, 'left'))
-hotkeys:bind({'ctrl'}, '.', keyBind({'alt'}, 'down'))
-hotkeys:bind({'ctrl'}, "'", keyBind({'alt'}, 'forwarddelete'))
-hotkeys:bind({'ctrl'}, ';', keyBind({'alt'}, 'delete'))
-hotkeys:bind({'ctrl'}, 'u', keyBind({'cmd'}, 'delete'))
-
--- Define a callback function to be called when application events happen
-function applicationWatcherCallback(appName, eventType, appObject)
-  if (appName == "Emacs") then
-    if (eventType == hs.application.watcher.activated) then
-      -- Emacs just got focus, disable our hotkeys
-      hotkeys:exit()
-    elseif (eventType == hs.application.watcher.deactivated) then
-      -- Emacs just lost focus, enable our hotkeys
-      hotkeys:enter()
-    end
-  end
-end
-
--- Create and start the application event watcher
-watcher = hs.application.watcher.new(applicationWatcherCallback)
-watcher:start()
-
--- Activate the modal state
-hotkeys:enter()
 
 -- [ Application Launcher ] ---------------------------------------------------
 
@@ -92,7 +55,51 @@ for key, app in pairs(appList) do
 	hs.hotkey.bind({'ctrl', 'cmd'}, key, function() hs.application.launchOrFocus(app) end)
 end
 
+
+-- [ Mode Test ] ---------------------------------------------------
+
+function modeTestStart()
+   hs.alert.show("Mode Entered")
+   testModal:enter()
+end
+function modeTestEnd()
+   hs.alert.show("Mode Exited")
+   testModal:exit()
+end
+
+testModal = hs.hotkey.modal.new({'cmd', 'ctrl', 'alt'}, 'F18')
+testModal:bind({''}, 'e', keyStrike({'ctrl'}, 'e'))
+hs.hotkey.bind({'cmd', 'ctrl'}, '2', modeTestStart, modeTestEnd)
+
+
+-- [ Arrow Delete ] -------------------------------------------------------------
+
+-- This makes it so fn+mods+left/right acts as a 'delete' modifer.
+-- This way you can zip around with the arrow keys and add the 'fn' key to delete
+-- while your hands are still on the arrows.
+
+hs.hotkey.bind({},      'home', function() hs.eventtap.keyStroke({},      'delete') end)
+hs.hotkey.bind({'alt'}, 'home', function() hs.eventtap.keyStroke({'alt'}, 'delete') end)
+hs.hotkey.bind({'cmd'}, 'home', function() hs.eventtap.keyStroke({'cmd'}, 'delete') end)
+hs.hotkey.bind({},      'end',  function() hs.eventtap.keyStroke({},      'forwarddelete') end)
+hs.hotkey.bind({'alt'}, 'end',  function() hs.eventtap.keyStroke({'alt'}, 'forwarddelete') end)
+hs.hotkey.bind({'cmd'}, 'end',  function() hs.eventtap.keyStroke({'ctrl'}, 'k') end)
+
+
+-- [ Word Move/Delete ] -------------------------------------------------------------
+
+-- This, remember, is at the system level, so Terminal/Emacs will need to accept
+-- things like alt+delete and cmd+delete.
+
+hs.hotkey.bind({'ctrl'}, 'w', function() hs.eventtap.keyStroke({'alt'}, 'delete') end)
+hs.hotkey.bind({'ctrl'}, 'u', function() hs.eventtap.keyStroke({'cmd'}, 'delete') end)
+hs.hotkey.bind({'ctrl'}, ';', function() hs.eventtap.keyStroke({'alt'}, 'left') end)
+hs.hotkey.bind({'ctrl'}, "'", function() hs.eventtap.keyStroke({'alt'}, 'right') end)
+hs.hotkey.bind({'ctrl'}, ',', function() hs.eventtap.keyStroke({'alt'}, 'delete') end)
+hs.hotkey.bind({'ctrl'}, '.', function() hs.eventtap.keyStroke({'alt'}, 'forwarddelete') end)
+
+
 -- [ Run at End ] -------------------------------------------------------------
 
--- This runs when this file is loaded
+-- This runs when this file is loaded to confirm it is working
 hs.notify.new({title='Hammerspoon', informativeText='Ready to rock 🤘'}):send()

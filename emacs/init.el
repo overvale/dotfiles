0cc77347c9900e9b367382e8661ba17590342920
Oliver Taylor <olivertaylor@me.com>
2020-08-11 12:54:40 -0700

    Adopts use-package, moves a few things around

--- emacs/init.el
+++ emacs/init.el
@@ -1,17 +1,24 @@
-;;; PACKAGES
-;; setup package system
 (require 'package)
+
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("org" . "https://orgmode.org/elpa/")
         ;;("melpa" . "https://melpa.org/packages/")
         ("melpa-stable" . "http://stable.melpa.org/packages/")
         ))
+
 (package-initialize)
 
-(setq vc-follow-symlinks t)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
+;; Load use-package
+(eval-when-compile (require 'use-package))
+
+;; Load org and load your config file
 (require 'org)
 (org-babel-load-file (expand-file-name "~/dot/emacs/emacs-init.org"))
+
 
 ;; init.el ends here

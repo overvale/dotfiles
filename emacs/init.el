a6cf4ecd0d053c9859021ec6206414b5e8e9e3f2
Oliver Taylor <olivertaylor@me.com>
2020-08-21 13:06:11 -0700

    Misc changes to improve startup time 6.96s => 1.36s WINNING

--- emacs/init.el
+++ emacs/init.el
@@ -7,16 +7,36 @@
         ("melpa-stable" . "http://stable.melpa.org/packages/")
         ))
 
-(package-initialize)
+;; Initialise the packages, avoiding a re-initialisation.
+(unless (bound-and-true-p package--initialized)
+  (setq package-enable-at-startup nil)
+  (package-initialize))
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
 ;; Load use-package
-(eval-when-compile (require 'use-package))
+(eval-when-compile
+  (require 'use-package))
+
+(use-package vc
+  :config
+  (setq vc-follow-symlinks t)) ; Because my dotfiles are managed that way
+
+;; If the el file is newer than the org file, load that instead of using
+;; org-babel, which loads org, which takes a few seconds. This is done to speed
+;; startup time.
+;;(let* ((conf "~/dot/emacs/emacs-init")
+;;       (el (concat conf ".el"))
+;;       (org (concat conf ".org")))
+;;  (when (or (not (file-exists-p el))
+;;            (file-newer-than-file-p org el))
+;;    (org-babel-load-file org))
+;;      (load-file el))
+
 
 ;; Load org and load your config file
 (org-babel-load-file (expand-file-name "~/dot/emacs/emacs-init.org"))
 
-;; init.el ends here
+;; ------------------------------------------------------------------

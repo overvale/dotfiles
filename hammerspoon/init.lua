34b28133884529b1c68bac38912dd0ce3c89f2a5
Oliver Taylor <olivertaylor@me.com>
2020-08-23 16:46:50 -0700

    Improves window control, inspired by @raulchen's hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,18 +1,26 @@
 --[[ INFO ----------------------------------------
 
-http://github.com/jasonrudolph/keyboard
-http://github.com/dbmrq/dotfiles/
+KeyCodes:
+f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15,
+f16, f17, f18, f19, f20, pad, pad*, pad+, pad/, pad-, pad=,
+pad0, pad1, pad2, pad3, pad4, pad5, pad6, pad7, pad8, pad9,
+padclear, padenter, return, tab, space, delete, escape, help,
+home, pageup, forwarddelete, end, pagedown, left, right, down, up,
+shift, rightshift, cmd, rightcmd, alt, rightalt, ctrl, rightctrl,
+capslock, fn
 
-See 'hs.keycodes.map' for keycodes.
-
-Check out:
-- ControlEscape.spoon
+Inspiration:
 - http://www.hammerspoon.org/Spoons/Seal.html
 - https://medium.com/@robhowlett/hammerspoon-the-best-mac-software-youve-never-heard-of-40c2df6db0f8
+- https://github.com/jasonrudolph/keyboard
+- https://github.com/dbmrq/dotfiles
+- https://github.com/raulchen/dotfiles
+- https://github.com/justintanner/universal-emacs-keybindings
 
-]]
+]] ---------------------------------------------------
 
 hs.window.animationDuration = 0
+-- TODO: file bug for this setting not always taking effect
 local hyper = {"ctrl", "alt", "cmd"}
 
 -- Spoons
@@ -39,6 +47,7 @@ vim:shouldDimScreenInNormalMode(nil)
 
 require("smart_modifier_keys")
 -- TODO:
+-- This doesn't work reliably.
 -- You could create a modal hotkey that enters an "emacs god mode"
 -- wherein all pressed keys have a ctrl modifier added to them.
 
@@ -71,18 +80,20 @@ hs.hotkey.bind(hyper, '=', function() hs.window.centerOnScreen(hs.window.focused
 
 -- Resize and Move Windows
 -- It seems you have to press the keys a few times to get them to work
-hs.hotkey.bind(hyper, 'left', function() hs.window.focusedWindow():moveToUnit({0, 0, 0.5, 1}) end)
-hs.hotkey.bind(hyper, 'right', function() hs.window.focusedWindow():moveToUnit({0.5, 0, 0.5, 1}) end)
-hs.hotkey.bind(hyper, 'h', function() hs.window.focusedWindow():moveToUnit({0, 0, 0.333, 1}) end)
-hs.hotkey.bind(hyper, 'j', function() hs.window.focusedWindow():moveToUnit({0, 0, 0.666, 1}) end)
-hs.hotkey.bind(hyper, 'k', function() hs.window.focusedWindow():moveToUnit({0.333, 0, 0.666, 1}) end)
-hs.hotkey.bind(hyper, 'l', function() hs.window.focusedWindow():moveToUnit({0.666, 0, 0.333, 1}) end)
+hs.hotkey.bind(hyper, 'left', function() hs.window.focusedWindow():moveToUnit({0, 0, 1/2, 1}) end)
+hs.hotkey.bind(hyper, 'right', function() hs.window.focusedWindow():moveToUnit({1/2, 0, 1/2, 1}) end)
+hs.hotkey.bind(hyper, 'h', function() hs.window.focusedWindow():moveToUnit({0, 0, 1/3, 1}) end)
+hs.hotkey.bind(hyper, 'j', function() hs.window.focusedWindow():moveToUnit({0, 0, 2/3, 1}) end)
+hs.hotkey.bind(hyper, 'k', function() hs.window.focusedWindow():moveToUnit({1/3, 0, 2/3, 1}) end)
+hs.hotkey.bind(hyper, 'l', function() hs.window.focusedWindow():moveToUnit({2/3, 0, 1/3, 1}) end)
+-- TODO: file bug for not all of these being applied on first hotkey press
 
 -- window hints
-hs.hotkey.bind(hyper, 'h', hs.hints.windowHints)
+hs.hotkey.bind(hyper, 'i', hs.hints.windowHints)
 
--- grid gui
-hs.grid.setMargins({w = 0, h = 0})
+-- window grid
+hs.grid.setGrid('6x4', nil, nil)
+hs.grid.setMargins({0, 0})
 hs.hotkey.bind(hyper, 'g', hs.grid.show)
 
 
@@ -157,29 +168,28 @@ hs.hotkey.bind({'ctrl'}, ';', function() hs.eventtap.keyStroke({'ctrl', 'alt'}, 
 hs.hotkey.bind({'ctrl'}, "'", function() hs.eventtap.keyStroke({'ctrl', 'alt'}, 'f') end)
 hs.hotkey.bind({'alt'}, 'd', function() hs.eventtap.keyStroke({'alt'}, 'forwarddelete') end)
 
-
-
 -- Window Switcher
 -- -----------------------------------------------
--- https://github.com/manateelazycat/hammerspoon-config/blob/master/init.lua
+-- https://github.com/raulchen/dotfiles/tree/master/hammerspoon
 
-switcher = hs.window.switcher.new(
-    hs.window.filter.new()
-        :setAppFilter('Emacs', {allowRoles = '*', allowTitles = 1}), -- make emacs window show in switcher list
-    {
-        showTitles = false,               -- don't show window title
-        thumbnailSize = 200,              -- window thumbnail size
-        showSelectedThumbnail = false,    -- don't show bigger thumbnail
-        backgroundColor = {0.3,0.3,0.3,1}, -- background color
-        highlightColor = {0.8,0.8,0.8,1}, -- selected color
-    }
-)
+local switcher = hs.window.switcher.new(nil, {
+    fontName = ".AppleSystemUIFont",
+    textSize = 16,
+    textColor = { white = 0, alpha = 1 },
+    highlightColor = { white = 0.5, alpha = 0.3 },
+    backgroundColor = { white = 0.95, alpha = 0.9 },
+    titleBackgroundColor = { white = 0.95, alpha = 0 },
+    showThumbnails = false,
+    showSelectedThumbnail = false,
+})
 
-hs.hotkey.bind("alt", "tab", function()
-                   switcher:next()
-                   updateFocusAppInputMethod()
-end)
-hs.hotkey.bind("alt-shift", "tab", function()
-                   switcher:previous()
-                   updateFocusAppInputMethod()
-end)
+local function nextWindow()
+    switcher:next()
+end
+
+local function previousWindow()
+    switcher:previous()
+end
+
+hs.hotkey.bind('alt', 'tab', nextWindow, nil, nextWindow)
+hs.hotkey.bind('alt-shift', 'tab', previousWindow, nil, previousWindow)

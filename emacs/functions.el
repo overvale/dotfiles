152d104bf376ac37a880557a37cc0266c6126938
Oliver Taylor <olivertaylor@me.com>
2020-09-02 21:09:33 -0700

    Adds function and keybindings for selectrum-outline
    This presents headlines from outline-mode in selectrum. Pretty much a clone of
    how counsel-outline works.

--- emacs/functions.el
+++ emacs/functions.el
@@ -216,6 +216,7 @@ URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'"
   (interactive)
   (forward-word 2)
   (backward-word))
+
 (defun recentf-open-files+ ()
   "Use `completing-read' to open a recent file."
   (interactive)
@@ -356,3 +357,139 @@ and the point, not include the isearch word."
   (interactive)
   (org-insert-time-stamp (current-time))
   )
+
+(defvar selectrum-outline-history nil "History of chosen headings for `selectrum-outline'.")
+(defun selectrum-outline ()
+  "Jump to a heading.  Regexps are pre-defined.  Obeys narrowing."
+  (interactive)
+  (let ((selectrum-should-sort-p nil)) ; Headings should stay in order of appearance.
+    ;; Just use Org's built-in features when applicable.
+    (if (eq major-mode 'org-mode)
+        (let ((org-outline-path-complete-in-steps)
+              (org-goto-interface 'outline-path-completion))
+          (org-goto))
+
+      ;; Otherwise, have to find and format headings manually.
+      (let* ((heading-regexp
+              (cl-case major-mode
+                ;; Groups: (1) level determinant, (2) heading text.
+                ;; The top level is 0, for a zero-length determinant.
+                (emacs-lisp-mode
+                 "^;;;\\(?1:;*\\)[[:blank:]]*\\(?2:[[:alnum:]][^z-a]*\\)\\'")
+                (markdown-mode
+                 "^#\\(?1:#*\\)[[:blank:]]*\\(?2:[[:alnum:]][^z-a]*\\)\\'")
+                (python-mode
+                 "^##\\(?1:\\**\\|#*\\)[[:blank:]]*\\(?2:[[:alnum:]][^z-a]*\\)\\'")
+                (t
+                 (user-error "selectrum-outline: No headings defined for %s."
+                             major-mode))))
+
+             ;; Get the basic information of each heading in the accessible
+             ;; portion of the buffer.
+             (buffer-contents (split-string (buffer-string) "\n"))
+             (headings
+              (cl-loop for txt in buffer-contents
+                       for num from 1 to (1- (length buffer-contents))
+                       ;; Only get the heading lines.
+                       when (string-match heading-regexp txt)
+                       ;; Heading text, Outline level, Line number
+                       collect (list (match-string-no-properties 2 txt)
+                                     (length (match-string-no-properties 1 txt))
+                                     num)))
+
+             ;; Create the prefix headings ("H1", "H1/h2", etc.)
+             (formatted-headings
+              (cl-loop
+               ;; Get current line, used to find closest heading.
+               ;; We're not moving point, so `save-excursion' isn't needed
+               ;; anywhere.
+               with current-line-number = (line-number-at-pos (point))
+               ;; Create a line-number format.
+               ;; There are a few reasons to include the line number at the
+               ;; beginning of the candidate:
+               ;; 1. Emacs won't show duplicate candidates, so we need a way to
+               ;     distinguish headings with the same full path
+               ;     (such as 2 candidates of "a/b/c").
+               ;; 2. In cases where headings are duplicated, it adds context.
+               ;; 3. `completing-read' won't return candidates with their
+               ;;    text properties,and we need a way to associate a heading
+               ;;    with its line number.
+               ;;
+               ;; #3 could be avoided by using `selectrum-read', and makes for
+               ;; a slightly more comfortable experience, but then the command
+               ;; wouldn't work in Icomplete and others.
+               with number-format = (format "L%%0%dd: "
+                                            (length (number-to-string
+                                                     (length buffer-contents))))
+               ;; Variables for keeping track of heading "path".
+               with backwards-prefix-list = ()
+               with prev-heading-level = 0
+               for prev-heading-text  = nil then heading-text
+               for (heading-text heading-level line-num) in headings
+               do (cond
+                   ;; If we've moved to a greater level (further down the tree),
+                   ;; add the previous heading to the heading prefix list so
+                   ;; that we can prepend it to the current heading when
+                   ;; formatting.
+                   ((> heading-level prev-heading-level)
+                    (setq backwards-prefix-list (cons prev-heading-text
+                                                      backwards-prefix-list)
+                          prev-heading-level heading-level))
+                   ;; Otherwise, if we've moved to a lower level (higher up the
+                   ;; tree), and need to remove the most recently added prefix
+                   ;; from the list (i.e., go from '(c b a) back to '(b a)).
+                   ((< heading-level prev-heading-level)
+                    (setq backwards-prefix-list (last backwards-prefix-list
+                                                      heading-level)
+                          prev-heading-level heading-level))
+                   ;; Otherwise, do nothing.
+                   (t nil))
+               collect (cons (concat
+                              (format number-format line-num)
+                              (string-join (reverse backwards-prefix-list)
+                                           "/")
+                              (and backwards-prefix-list "/")
+                              heading-text)
+                             ;; Find distance from the heading line to the
+                             ;; current line.  Positive for lines after, and
+                             ;; negative for lines before.
+                             (- line-num current-line-number))))
+
+             ;; Get default candidate.
+             (default-candidate
+               (cl-loop for prev-heading = "" then heading
+                        for (heading . dist) in formatted-headings
+                        ;; Once we've gone down past the current line, return
+                        ;; the previous heading (the one found before going
+                        ;; past).
+                        when (> dist 0)
+                        return prev-heading))
+
+             ;; Get the desired heading.
+             (chosen-heading (completing-read "Jump to heading: "
+                                              formatted-headings
+                                              nil t nil
+                                              'selectrum-outline-history
+                                              default-candidate))
+             ;; Stop at the ":". It is followed by one " ".
+             (line-number-prefix (seq-take-while (lambda (char)
+                                                   (not (char-equal ?: char)))
+                                                 chosen-heading))
+             ;; Get the line number for that heading, skipping the "L" in
+             ;; line-number-prefix.
+             (chosen-line-number
+              (string-to-number (substring line-number-prefix 1)))
+             ;; Get the current line number to determine the travel distance.
+             (current-line-number (line-number-at-pos (point))))
+
+        ;; After selection, manually edit history to remove line numbers.
+        (setcar selectrum-outline-history
+                (substring chosen-heading
+                           ;; Want after line-prefix followed by ": ".
+                           (+ (length line-number-prefix) 2)))
+        ;; Now do the actual movement, but first push mark.
+        (push-mark (point) t)
+        ;; Using `goto-line' isn't recommended for non-interactive use.
+        (forward-line (- chosen-line-number current-line-number))
+        (beginning-of-line-text 1)))))
+

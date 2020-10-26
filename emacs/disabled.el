c630221a24859af51fbdb3c041e1a577ee59c78c
Oliver Taylor <olivertaylor@me.com>
2020-10-25 19:18:59 -0700

    Adds smart-escape to 'disabled'
    Actually works very well, but breaks something I rely on. When doing a search
    with ctrl-f pressing 'esc' no longer takes you back to where you initiated the
    search from. So I'm adding it to disabled. Maybe one day I can fix it.

--- emacs/disabled.el
+++ emacs/disabled.el
@@ -262,3 +262,45 @@ _~_: modified
 
 ;; Show "View" in mode-line when in 'view-mode'
 (setq minions-direct '(view-mode))
+
+
+;;; Smart Escape
+
+;; Ever try to quit the minibuffer while the point is in another window? This
+;; function makes that possible.
+;; https://with-emacs.com/posts/tips/quit-current-context/
+
+(defun keyboard-quit-context+ ()
+  "Quit current context.
+
+This function is a combination of `keyboard-quit' and
+`keyboard-escape-quit' with some parts omitted and some custom
+behavior added."
+  (interactive)
+  (cond ((region-active-p)
+         ;; Avoid adding the region to the window selection.
+         (setq saved-region-selection nil)
+         (let (select-active-regions)
+           (deactivate-mark)))
+        ((eq last-command 'mode-exited) nil)
+        (current-prefix-arg
+         nil)
+        (defining-kbd-macro
+          (message
+           (substitute-command-keys
+            "Quit is ignored during macro defintion, use \\[kmacro-end-macro] if you want to stop macro definition"))
+          (cancel-kbd-macro-events))
+        ((active-minibuffer-window)
+         (when (get-buffer-window "*Completions*")
+           ;; hide completions first so point stays in active window when
+           ;; outside the minibuffer
+           (minibuffer-hide-completions))
+         (abort-recursive-edit))
+        (t
+         ;; if we got this far just use the default so we don't miss
+         ;; any upstream changes
+         (keyboard-quit))))
+
+(global-set-key [remap keyboard-quit] #'keyboard-quit-context+)
+
+

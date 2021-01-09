18226de4695d498b995c6bf690b9852111b2f4c6
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2021-01-08 22:32:05 -0800

    Swaps completion framework for selectrum+prescient+marginalia

--- emacs/minimum.el
+++ emacs/minimum.el
@@ -71,27 +71,23 @@
   (whole-line-or-region-global-mode 1)
   )
 
-(use-package icomplete-vertical
-  :demand t
-  :custom
-  (completion-styles '(partial-completion substring))
-  (completion-category-overrides '((file (styles basic substring))))
-  (read-file-name-completion-ignore-case t)
-  (read-buffer-completion-ignore-case t)
-  (completion-ignore-case t)
-  :config
-  (icomplete-mode)
-  (icomplete-vertical-mode)
-  :bind (:map icomplete-minibuffer-map
-              ("<down>" . icomplete-forward-completions)
-              ("C-n" . icomplete-forward-completions)
-              ("<up>" . icomplete-backward-completions)
-              ("C-p" . icomplete-backward-completions)
-              ("C-v" . icomplete-vertical-toggle)))
+(use-package selectrum
+  :init
+  (selectrum-mode +1)
+  )
 
-(use-package orderless
-  :demand t
-  :custom (completion-styles '(orderless)))
+(use-package selectrum-prescient
+  :init
+  (selectrum-prescient-mode +1)
+  (prescient-persist-mode +1)
+  )
+
+(use-package marginalia
+  :init
+  (marginalia-mode 1)
+  (setq marginalia-annotators
+	'(marginalia-annotators-heavy marginalia-annotators-light))
+  )
 
 
 ;;; Keyboard modifiers setup

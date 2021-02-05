c5c76068d0b56c60af8886ad15ea43522a16b651
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2021-02-05 00:16:38 -0800

    Adds functions/bindings for using the mark
    Inspired by https://spwhitton.name/blog/entry/transient-mark-mode/

--- emacs/init.el
+++ emacs/init.el
@@ -578,6 +578,34 @@
   :hook (ibuffer-mode . oht-ibuffer-hook)
   )
 
+;;; Transient Mark
+;; https://www.masteringemacs.org/article/fixing-mark-commands-transient-mark-mode
+
+(use-package oht-transient-mark
+  :straight nil
+  :config
+  (defun oht-activate-or-swap-mark ()
+    "If no region is active, activate it. If a region is active swap the point and mark."
+    (interactive)
+    (if (use-region-p)
+	(exchange-point-and-mark)
+      (activate-mark)))
+  (defun push-mark-no-activate ()
+    "Pushes `point' to `mark-ring' and does not activate the region
+   Equivalent to \\[set-mark-command] when \\[transient-mark-mode] is disabled"
+    (interactive)
+    (push-mark (point) t nil)
+    (message "Pushed mark to ring"))
+  (defun jump-to-mark ()
+    "Jumps to the local mark, respecting the `mark-ring' order.
+  This is the same as using \\[set-mark-command] with the prefix argument."
+    (interactive)
+    (set-mark-command 1))
+  :bind
+  ("C-x C-x" . oht-activate-or-swap-mark)
+  ("C-`" . push-mark-no-activate)
+  ("M-`" . jump-to-mark)
+  )
 
 ;;; Keybindings
 

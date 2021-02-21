d418e87f7ca8645fe09addcbbb39ae26dc544459
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2021-02-20 21:30:47 -0800

    Moves use-selectrum/embark functions out of oht-embark

--- emacs/init.el
+++ emacs/init.el
@@ -371,6 +371,36 @@ variable-pitch and fixed-pitch fonts to always be 1.0."
   (load (concat oht-dotfiles "lisp/oht-embark.el"))
   )
 
+(defun use-embark-completions ()
+  "Use Embark for completions.
+
+These hooks make it so that the embark-live-collect buffer is
+automatically displayed after you type something. Keep in mind that this
+separate from listing all the potential completions when you press TAB.
+
+That means pressing TAB when there are multiple possible
+candidates (or is empty) will result in BOTH the
+embark-live-collect and *completions* buffers being shown. When
+there is only one candidate, however, TAB will complete."
+  (interactive)
+  (selectrum-mode -1)
+  (add-hook 'minibuffer-setup-hook 'embark-collect-completions-after-input)
+  (add-hook 'embark-post-action-hook 'embark-collect--update-linked)
+  (add-hook 'embark-collect-mode-hook 'hl-line-mode)
+  )
+
+(defun use-selectrum-completions ()
+  "Use Selectrum for completions.
+
+This simply removes the hooked added by the function `use-embark-completions'."
+  (interactive)
+  (selectrum-mode 1)
+  (remove-hook 'minibuffer-setup-hook 'embark-collect-completions-after-input)
+  (remove-hook 'embark-post-action-hook 'embark-collect--update-linked)
+  (remove-hook 'embark-collect-mode-hook 'hl-line-mode)
+  )
+
+
 (use-package consult
   :straight (:type git :host github :repo "minad/consult" :branch "main")
   :bind
--- emacs/lisp/oht-embark.el
+++ emacs/lisp/oht-embark.el
@@ -27,33 +27,4 @@
 		(shrink-window selectrum-num-candidates-displayed)
 		(setq-local selectrum-num-candidates-displayed 0)))))
 
-(defun use-embark-completions ()
-  "Use Embark for completions.
-
-These hooks make it so that the embark-live-collect buffer is
-automatically displayed after you type something. Keep in mind that this
-separate from listing all the potential completions when you press TAB.
-
-That means pressing TAB when there are multiple possible
-candidates (or is empty) will result in BOTH the
-embark-live-collect and *completions* buffers being shown. When
-there is only one candidate, however, TAB will complete."
-  (interactive)
-  (selectrum-mode -1)
-  (add-hook 'minibuffer-setup-hook 'embark-collect-completions-after-input)
-  (add-hook 'embark-post-action-hook 'embark-collect--update-linked)
-  (add-hook 'embark-collect-mode-hook 'hl-line-mode)
-  )
-
-(defun use-selectrum-completions ()
-  "Use Selectrum for completions.
-
-This simply removes the hooked added by the function `use-embark-completions'."
-  (interactive)
-  (selectrum-mode 1)
-  (remove-hook 'minibuffer-setup-hook 'embark-collect-completions-after-input)
-  (remove-hook 'embark-post-action-hook 'embark-collect--update-linked)
-  (remove-hook 'embark-collect-mode-hook 'hl-line-mode)
-  )
-
 (provide 'oht-embark)

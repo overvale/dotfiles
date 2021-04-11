5c4019926bae93b05aa25a40fe369e54df8ecf82
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2021-04-11 00:30:15 -0700

    Overhaul and simplification of oht-fonts

--- emacs/lisp/oht-fonts.el
+++ emacs/lisp/oht-fonts.el
@@ -4,7 +4,7 @@
 ;;; Functions
 
 (defun oht-fonts-line-spacing (&optional arg)
-  "Buffer local, set the line spacing. Takes an argument, 0 by default"
+  "Sets line spacing for the current buffer. Takes an argument, 0 by default"
   (interactive "P")
   (setq-local line-spacing arg))
 
@@ -24,11 +24,11 @@ This value will be multiplied by 10, so 12 will become 120. This is to comply
 with Emacs' set-face-attribute requirements.")
 
 (defvar oht-fonts-variable-size 14
-  "Font size, as an integer, to be used for the variable-pitch size.
+  "Font point size, as an integer, to be used for the variable-pitch size.
 
-This value will be used to determine a relative (float) size based on the default
-size. So if your default size is 12 and your variable size is 14 the derived
-size will be 1.16.")
+This value will be used to determine a relative (float) size based on the size
+of oht-fonts-monospace. So if your monospace size is 12 and your variable size
+is 14 the derived size will be 1.16.")
 
 (defvar oht-fonts-monospace-weight 'normal
   "Weight of both the default and fixed-pitch faces.")
@@ -39,7 +39,11 @@ size will be 1.16.")
 ;;; Set fonts and their sizes
 
 (defun oht-fonts-set ()
-  "Function for setting fonts."
+  "Function for setting the default, variable-, and fixed-pitch faces.
+
+All three faces will be set to exactly the same size, with the variable-
+and fixed-pitch faces as relative (float) sizes. This allows
+text-scale-adjust to work correctly."
   (interactive)
   (set-face-attribute 'default nil
 					  :family oht-fonts-monospace
@@ -57,54 +61,40 @@ size will be 1.16.")
 
 ;;; Larger Variable Pitch Mode
 
-;; A minor mode to scale the variable-pitch face up to the height defined in
-;; ‘oht-fonts-variable-size’ and the fixed-pitch face down to the height
-;; defined in ‘oht-fonts-monospace-size’. This mode needs to be enabled
-;; wherever you want to adjust face sizes, perhaps with a hook.
+(define-minor-mode larger-variable-pitch-mode
+  "Minor mode to optically adjust the variable-pitch face size.
 
-(make-variable-buffer-local
- (defvar larger-variable-pitch-mode-status nil
-   "Status of the larger-variable-pitch-mode"))
-
-(make-variable-buffer-local
- (defvar variable-pitch-remapping nil
-   "variable-pitch remapping cookie for larger-variable-pitch-mode."))
-
-(make-variable-buffer-local
- (defvar fixed-pitch-remapping nil
-   "fixed-pitch remapping cookie for larger-variable-pitch-mode"))
-
-(defun larger-variable-pitch-mode-toggle ()
-  (setq larger-variable-pitch-mode-status (not larger-variable-pitch-mode-status))
-  (if larger-variable-pitch-mode-status
+A minor mode to scale (in the current buffer) the variable-pitch
+face up to the height defined by ‘oht-fonts-variable-size’ and
+the fixed-pitch face down to the height defined by
+‘oht-fonts-monospace-size’. More info about this implementation
+see the elisp info manual under 'Face Remapping'."
+  :init-value nil
+  :lighter " V+"
+  (if larger-variable-pitch-mode
       (progn
-	(setq variable-pitch-remapping
-	      (face-remap-add-relative 'variable-pitch
-				       :height (/ (float oht-fonts-variable-size)
-						  (float oht-fonts-monospace-size))))
-	(setq fixed-pitch-remapping 
-	      (face-remap-add-relative 'fixed-pitch
-				       :height (/ (float oht-fonts-monospace-size)
-						  (float oht-fonts-variable-size))))
-	(force-window-update (current-buffer)))
+		(setq-local variable-pitch-remapping
+					(face-remap-add-relative 'variable-pitch
+											 :height (/ (float oht-fonts-variable-size)
+														(float oht-fonts-monospace-size))))
+		(setq-local fixed-pitch-remapping
+					(face-remap-add-relative 'fixed-pitch
+											 :height (/ (float oht-fonts-monospace-size)
+														(float oht-fonts-variable-size))))
+		(force-window-update (current-buffer)))
     (progn
       (face-remap-remove-relative variable-pitch-remapping)
       (face-remap-remove-relative fixed-pitch-remapping))))
 
-(define-minor-mode larger-variable-pitch-mode
-  "Minor mode to scale the variable- and fixed-pitch faces up and down."
-  :init-value nil
-  :lighter " V+"
-  (larger-variable-pitch-mode-toggle))
-
-(defun oht-fonts-buffer-face-hook ()
-  "Activate and deactivate larger-variable-pitch-mode minor mode."
+(defun oht-fonts-buffer-face-setup ()
+  "If buffer-face-mode is active, activate larger-variable-pitch-mode, otherwise disable it."
   (if buffer-face-mode
       (larger-variable-pitch-mode 1)
     (larger-variable-pitch-mode -1)))
 
 ;; Make this mode the default whenever entering buffer-face-mode
-(add-hook 'buffer-face-mode-hook 'oht-fonts-buffer-face-hook)
+(add-hook 'buffer-face-mode-hook 'oht-fonts-buffer-face-setup)
+
 
 
 (provide 'oht-fonts)

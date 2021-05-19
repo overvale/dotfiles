86c393d30c397df5e18af571930e3db9fe91b248
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2021-05-19 10:32:09 -0700

    Adds savehist config

--- emacs/init.el
+++ emacs/init.el
@@ -504,6 +504,11 @@ Keybindings you define here will take precedence."
   :init
   (vertico-mode))
 
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
 (use-package marginalia
   :custom
   (marginalia-annotators

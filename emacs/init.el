a7a1308ff9872c041644c30bff7facecce08587c
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2021-05-24 22:35:29 -0700

    Adds occur-dwim

--- emacs/init.el
+++ emacs/init.el
@@ -211,6 +211,19 @@
 
 ;;;;; Miscellaneous
 
+(defun occur-dwim ()
+  "Call `occur' with a sane default."
+  (interactive)
+  (push (if (region-active-p)
+            (buffer-substring-no-properties
+             (region-beginning)
+             (region-end))
+          (let ((sym (thing-at-point 'symbol)))
+            (when (stringp sym)
+              (regexp-quote sym))))
+        regexp-history)
+  (call-interactively 'occur))
+
 (defun toggle-window-split ()
   "Toggle window split from vertical to horizontal."
   (interactive)

6f79e26fa113e0fae88322882208d261a90dbd70
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2021-05-26 22:51:07 -0700

    Changes order of package repos

--- emacs/init.el
+++ emacs/init.el
@@ -18,9 +18,9 @@
 
 (custom-set-variables
  '(package-archives
-   '(("gnu" . "https://elpa.gnu.org/packages/")
+   '(("org" . "https://orgmode.org/elpa/")
      ("melpa" . "https://melpa.org/packages/")
-     ("org" . "https://orgmode.org/elpa/"))))
+     ("gnu" . "https://elpa.gnu.org/packages/"))))
 
 (require 'package)
 

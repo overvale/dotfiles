3ba938b1b673283572c10813caeadb7acd7b00ef
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2021-10-09 22:53:46 -0700

    Adds quick-help macro

--- emacs/init.el
+++ emacs/init.el
@@ -1352,4 +1352,50 @@ current HH:MM time."
       ("]" "Backward Node" Info-forward-node)]]))
 
 
+;;; Quick Help
+
+;; This idea was stolen from the beautiful config here:
+;; https://svn.red-bean.com/repos/kfogel/trunk/.emacs
+;; This macro creates functions that display help text in a pop-up
+;; buffer.
+
+(defmacro quick-help (name buffer text)
+  "Macro for creating callable functions that display help.
+Where NAME is name of function, BUFFER is name of buffer, and TEXT is displayed."
+  (declare (indent defun))
+  `(progn
+     (defun ,name nil
+       ,buffer
+       (interactive)
+       (let ((qh-buff (concat "*Quick Help: " ,buffer "*"))
+             (qh-text ,text))
+         (get-buffer-create qh-buff)
+         (with-current-buffer qh-buff
+           (insert qh-text)
+           (goto-char (point-min))
+           (not-modified)
+           (read-only-mode)
+           (local-set-key (kbd "C-g") (lambda () (interactive) (other-window -1)))
+           (local-set-key (kbd "q") 'kill-buffer-and-window))
+         (pop-to-buffer qh-buff '((display-buffer-below-selected)
+                                  (window-parameters . ((no-other-window . nil)))
+                                  (window-height . fit-window-to-buffer)))
+         (message "C-g - Previous Window, q - Remove Window")))))
+
+;; And now the help you need...
+
+(quick-help qh--help-test
+  "Help Test"
+  "\
+This is helpful text.")
+
+(define-prefix-command 'quick-help-prompt nil "Quick Help")
+
+(let ((map quick-help-prompt))
+  (define-key map "p" '("Prod Agenda" . qh--prod-agenda)))
+
+(global-set-key (kbd "C-c h") 'quick-help-prompt)
+
+
+
 ;;; End of init.el

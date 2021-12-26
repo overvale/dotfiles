e7d89596207f13f7162962467fd9689a0fd6191a
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2021-12-26 04:48:30 -0800

    Cleans-up how 'Settings' are handled

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -11,10 +11,10 @@
 
 ;; Set these settings before the GUI frame is created.
 
-(custom-set-variables
- '(tool-bar-mode nil)
- '(scroll-bar-mode nil)
- '(menu-bar-mode t))
+(setq tool-bar-mode nil
+      menu-bar-mode t)
+
+(scroll-bar-mode 0)
 
 ;; Switch off garbage collection (will be switched on later).
 ;; This is step 1 of 2. Step 2 is in init.
--- emacs/init.el
+++ emacs/init.el
@@ -84,55 +84,12 @@
         visual-regexp-steroids))
 
 
-;;; Settings
-
-;; Save all interactive customization to a temp file, which is never loaded.
-;; This means interactive customization is session-local. Only this init file persists sessions.
-(setq custom-file (make-temp-file "emacs-custom-"))
-
-;; For most of my "settings" I use custom-set-variables, which does a bunch of neat stuff.
-;; First, it calls a variable's "setter" function, if it has one.
-;; Second, it can activate modes as well as set variables.
-;; Third, it takes care of setting the default for buffer-local variables correctly.
-;; https://with-emacs.com/posts/tutorials/almost-all-you-need-to-know-about-variables/#_user_options
-;; https://old.reddit.com/r/emacs/comments/exnxha/withemacs_almost_all_you_need_to_know_about/fgadihl/
-(custom-set-variables
- '(inhibit-startup-screen t)
- '(global-auto-revert-mode t)
- '(save-place-mode t)
- '(recentf-mode t)
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil)
- '(ring-bell-function 'ignore)
- '(pixel-scroll-precision-mode 1)
- '(scroll-step 1)
- '(set-language-environment "UTF-8")
- '(frame-title-format '("%b"))
- '(uniquify-buffer-name-style 'forward)
- '(vc-follow-symlinks t)
- '(find-file-visit-truename t)
- '(create-lockfiles nil)
- '(make-backup-files nil)
- '(bookmark-save-flag 1)
- '(bookmark-menu-confirm-deletion t)
- '(describe-bindings-outline t)
- '(word-wrap t)
- '(truncate-lines t)
- '(save-interprogram-paste-before-kill t)
- '(kill-do-not-save-duplicates t)
- '(kill-ring-max 512)
- '(sentence-end-double-space nil)
- '(tab-width 4)
- '(indent-tabs-mode nil)
- '(fill-column 78)
- '(delete-by-moving-to-trash t)
- '(trash-directory "~/.Trash"))
-
-
 ;;; Critical Setup
 
 ;; There are 2 ways I install packages/lisp that aren't a part of this file:
+;;
 ;; 1. Using `package-install-selected-packages',
+;;
 ;; 2. in a "local-package" directory that contains cloned git repositories
 ;;    that I can't install from a package archive such as elpa.gnu.org.
 ;;
@@ -197,6 +154,57 @@ Keybindings you define here will take precedence."
   (autoload 'transient-define-prefix "transient" nil t)
   (setq transient-detect-key-conflicts t
         transient-show-popup t))
+
+
+;;; Settings
+
+;; Save all interactive customization to a temp file, which is never loaded.
+;; This means interactive customization is session-local. Only this init file persists sessions.
+(setq custom-file (make-temp-file "emacs-custom-"))
+
+;; For most of my "settings" I use `custom-set-variables', which does a bunch of neat stuff:
+;;
+;; 1. It calls a variable's "setter" function, if it has one. A perfect
+;;    example of why this is needed is `truncate-lines', which simply will not
+;;    work unless set this way.
+;;
+;; 2. It can activate modes as well as set variables, which makes it tidy.
+;;
+;; 3. It takes care of setting the default for buffer-local variables correctly.
+;;
+;; https://with-emacs.com/posts/tutorials/almost-all-you-need-to-know-about-variables/#_user_options
+;; https://old.reddit.com/r/emacs/comments/exnxha/withemacs_almost_all_you_need_to_know_about/fgadihl/
+
+(custom-set-variables
+ '(inhibit-startup-screen t)
+ '(global-auto-revert-mode t)
+ '(save-place-mode t)
+ '(recentf-mode t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(ring-bell-function 'ignore)
+ '(pixel-scroll-precision-mode 1)
+ '(scroll-step 1)
+ '(set-language-environment "UTF-8")
+ '(frame-title-format '("%b"))
+ '(uniquify-buffer-name-style 'forward)
+ '(vc-follow-symlinks t)
+ '(find-file-visit-truename t)
+ '(create-lockfiles nil)
+ '(make-backup-files nil)
+ '(bookmark-save-flag 1)
+ '(bookmark-menu-confirm-deletion t)
+ '(describe-bindings-outline t)
+ '(truncate-lines t)
+ '(save-interprogram-paste-before-kill t)
+ '(kill-do-not-save-duplicates t)
+ '(kill-ring-max 512)
+ '(sentence-end-double-space nil)
+ '(tab-width 4)
+ '(indent-tabs-mode nil)
+ '(fill-column 78)
+ '(delete-by-moving-to-trash t)
+ '(trash-directory "~/.Trash"))
 
 
 ;;; Functions

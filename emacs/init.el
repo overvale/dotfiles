823a3e2cf9ee41587d69fff694f1f6b4b91a735a
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2022-01-02 08:59:46 -0800

    Removes kill-region advice

--- emacs/init.el
+++ emacs/init.el
@@ -513,15 +513,6 @@ This will save the buffer if it is not currently saved."
 (advice-add 'indent-rigidly-left-to-tab-stop :after 'activate-mark)
 (advice-add 'indent-rigidly-right-to-tab-stop :after 'activate-mark)
 
-(defadvice kill-region (before unix-werase activate compile)
-  "When called interactively with no active region, delete a single word
-backwards instead."
-  (interactive
-   (if mark-active (list (region-beginning) (region-end))
-     (list (save-excursion (backward-word 1) (point)) (point)))))
-
-
-
 
 ;;; Minibuffer
 

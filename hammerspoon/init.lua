91644e59f52dcb46912d1aad3c64d6933a1f6a26
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2022-09-30 20:15:12 -0700

    Removes SkyRocket Spoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -92,25 +92,8 @@ end
 hs.hotkey.bind(hyper, "1", toggleMute)
 
 
--- Sky Rocket Spoon -- Window Resizing with Mouse
--- -----------------------------------------------
 
-local SkyRocket = hs.loadSpoon("SkyRocket")
 
-sky = SkyRocket:new({
-  -- Opacity of resize canvas
-  opacity = 0.25,
-
-  -- Which mouse button to hold to move a window?
-  moveMouseButton = 'left',
-  -- Which modifiers to hold to move a window?
-  moveModifiers = {'shift', 'ctrl'},
-
-  -- Which mouse button to hold to resize a window?
-  resizeMouseButton = 'left',
-  -- Which modifiers to hold to resize a window?
-  resizeModifiers = {'cmd', 'alt'},
-})
 
 
 -- Readline Shortcuts

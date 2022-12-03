ce4ff3458b4e69f29539ddbadf5bb442ef020fcd
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2022-12-03 12:05:33 -0800

    Bring all Teams windows to front, instead of Finder

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -291,8 +291,7 @@ end
 local function appWatcherFunction(appName, eventType, appObject)
    if (eventType == hs.application.watcher.activated) then
       setUserKeymaps()
-      if (appName == "Finder") then
-         -- Bring all Finder windows forward when one gets activated
+      if (appName == "Microsoft Teams") then
          appObject:selectMenuItem({"Window", "Bring All to Front"})
       end
    end

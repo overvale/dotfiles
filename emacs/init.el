85678cd65ea8709819d49dc6c0636c8453680256
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2023-01-26 22:21:08 -0800

    Updates org-mode settings

--- emacs/init.el
+++ emacs/init.el
@@ -1790,6 +1790,25 @@ See also: https://stackoverflow.com/questions/9547912/emacs-calendar-show-more-t
 
 ;;; Org
 
+(setq org-directory (concat outpost-dir "notes/"))
+
+(setq org-agenda-files '("~/home/outpost/notes/todo.org"
+                         "~/home/checkins.org"))
+
+(setq org-capture-templates '(("t" "New Outpost Task"
+                               checkitem
+                               (file+headline "~/home/outpost/notes/todo.org" "Active"))))
+
+(defkey override-global-map
+  "s-k" 'org-capture)
+
+(add-hook 'org-mode-hook 'variable-pitch-mode)
+
+(defun org-search nil
+  "Go to any headline in your refile targets."
+  (interactive)
+  (org-refile '(4)))
+
 (with-eval-after-load 'org
   (custom-set-variables
    '(org-list-allow-alphabetical t)
@@ -1799,15 +1818,16 @@ See also: https://stackoverflow.com/questions/9547912/emacs-calendar-show-more-t
    '(org-special-ctrl-k t)
    '(org-return-follows-link t)
    '(org-catch-invisible-edits 'show-and-error)
-   '(org-outline-path-complete-in-steps nil)
    '(org-refile-use-outline-path 'file)
-   '(org-refile-targets '((org-agenda-files :maxlevel . 2)))
+   '(org-outline-path-complete-in-steps nil)
+   '(org-goto-interface 'outline-path-completion)
+   '(org-goto-max-level 10)
+   '(org-refile-targets '((org-agenda-files :maxlevel . 5)))
    '(org-startup-with-inline-images t)
    '(org-image-actual-width '(600))
    '(org-hide-emphasis-markers nil)
    '(org-hide-leading-stars nil)
    '(org-adapt-indentation nil)
-   '(org-ellipsis " 􀐷")
    '(org-insert-heading-respect-content t)
    '(org-list-demote-modify-bullet '(("+" . "*") ("*" . "-") ("-" . "+")))
    '(org-list-indent-offset 2)
@@ -1818,20 +1838,93 @@ See also: https://stackoverflow.com/questions/9547912/emacs-calendar-show-more-t
    '(org-src-tab-acts-natively t)
    '(org-edit-src-content-indentation 0))
 
+  (transient-define-prefix org-todo-transient ()
+    [["Org TODO Status"
+      ("t" "TODO"     (lambda () (interactive) (org-todo "TODO")))
+      ("w" "WAIT"     (lambda () (interactive) (org-todo "DELG")))
+      ("l" "LATER"    (lambda () (interactive) (org-todo "LATER")))
+      ("d" "DONE"     (lambda () (interactive) (org-todo "DONE")))
+      ("c" "CANCELED" (lambda () (interactive) (org-todo "CANCELED")))
+      ("m" "MOVED"    (lambda () (interactive) (org-todo "MOVED")))
+      ]])
+
+  (defkey org-mode-map "C-c C-t" 'org-todo-transient)
+
   (add-to-list 'org-structure-template-alist '("L" . "src emacs-lisp"))
   (add-to-list 'org-structure-template-alist '("f" . "src fountain"))
 
   (setq org-todo-keywords
-        '((sequence "TODO" "DELG" "LATER" "|" "DONE" "MOVED" "CANCELED")))
+        '((sequence "TODO" "WAIT" "LATER" "|" "DONE" "MOVED" "CANCELED")))
 
   (setq org-todo-keyword-faces
-        '(("DELG" . org-scheduled-previously)
-          ("LATER" . org-scheduled-previously)))
+        '(("WAIT" . org-scheduled-previously)
+          ("LATER" . org-scheduled-previously))))
 
-  (defun org-toggle-checkbox-presence ()
-    (interactive)
-    (let ((current-prefix-arg '(4)))
-      (call-interactively 'org-toggle-checkbox))))
+
+(with-eval-after-load 'org-agenda
+  (custom-set-variables
+   '(org-agenda-window-setup 'current-window)
+   '(org-agenda-restore-windows-after-quit t)
+   '(org-agenda-span 'week)
+   '(org-agenda-start-with-log-mode nil)
+   '(org-agenda-log-mode-items '(closed clock state))
+   '(org-agenda-use-time-grid nil)
+   '(org-deadline-warning-days 7)
+   '(org-agenda-todo-ignore-scheduled nil)
+   '(org-agenda-todo-ignore-deadlines nil)
+   '(org-agenda-skip-deadline-if-done t)
+   '(org-agenda-skip-scheduled-if-done t)
+   '(org-agenda-skip-deadline-prewarning-if-scheduled t))
+
+  (add-hook 'org-agenda-mode-hook 'hl-line-mode)
+
+  (transient-define-prefix org-agenda-todo-transient ()
+    [["Org TODO Status"
+      ("t" "TODO"     (lambda () (interactive) (org-agenda-todo "TODO")))
+      ("w" "WAIT"     (lambda () (interactive) (org-agenda-todo "DELG")))
+      ("l" "LATER"    (lambda () (interactive) (org-agenda-todo "LATER")))
+      ("d" "DONE"     (lambda () (interactive) (org-agenda-todo "DONE")))
+      ("c" "CANCELED" (lambda () (interactive) (org-agenda-todo "CANCELED")))
+      ("m" "MOVED"    (lambda () (interactive) (org-agenda-todo "MOVED")))
+      ]])
+
+  (defkey org-agenda-mode-map "t" 'org-agenda-todo-transient)
+
+  (setq org-agenda-custom-commands
+        '(("1" "Agenda + TODO"
+           ((agenda 'week)
+            (todo "TODO|WAIT"
+                  ((org-agenda-sorting-strategy '(todo-state-up priority-down))
+                   (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'deadline))
+                   (org-agenda-overriding-header "Active, not scheduled, Tasks: ")))))))
+
+  (setq org-agenda-prefix-format '((agenda . " %i %?-12t% s")
+                                   (todo . " %i %-12:c")
+                                   (tags . " %i %-12:c")
+                                   (search . " %i %-12:c")))
+
+  (defkey org-agenda-mode-map
+    "k" 'org-capture
+    "K" 'org-agenda-capture
+    "S" 'org-agenda-schedule
+    "D" 'org-agenda-deadline))
+
+(with-eval-after-load 'calendar
+  (defun org-calendar-capture (&optional with-time)
+    "Call `org-capture' with the date at point.
+With a `C-1' prefix, use the HH:MM value at point, if any, or the
+current HH:MM time."
+    (interactive "P")
+    (if (not (eq major-mode 'calendar-mode))
+        (user-error "You cannot do this outside of calendar buffers")
+      (progn
+        (require 'org)
+        (let ((org-overriding-default-time
+	           (org-get-cursor-date)))
+          (delete-window)
+          (call-interactively 'org-capture)))))
+
+  (defkey calendar-mode-map "k" 'org-calendar-capture))
 
 
 ;;; End of init.el

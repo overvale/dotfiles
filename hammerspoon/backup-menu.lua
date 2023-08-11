57d1be3e1c9d71af77ddb0e45a191427f61e9db6
Oliver Taylor <1388093+olivertaylor@users.noreply.github.com>
2023-08-11 08:00:54 -0700

    Adds hammerspoon backup-menu

---
+++ hammerspoon/backup-menu.lua
@@ -0,0 +1,27 @@
+-- Backup Menu
+-- --------------------------------------------------
+
+backupMenu = hs.menubar.new()
+
+function lastBackupNAS ()
+   local output, _, _ = hs.execute("/Users/oht/home/src/rsync-backup/last-backup-NAS.sh")
+   return output
+end
+
+function lastBackupCloud ()
+   local output, _, _ = hs.execute("/Users/oht/home/src/rsync-backup/last-backup-cloud.sh")
+   return output
+end
+
+function backupMenuItem()
+    local menuTable = {
+      { title = "Latest Backups:", disabled = true },
+      { title = lastBackupNAS(), disabled = true },
+      { title = lastBackupCloud(), disabled = true },
+    }
+    return menuTable
+end
+
+backupMenu:setMenu(backupMenuItem)
+
+backupMenu:setTitle("Backups")
